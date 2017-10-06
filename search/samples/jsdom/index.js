296df87eeeb5099ed1552181694fe20631a80bed
Sam Dutton <samdutton@gmail.com>
2017-10-06 17:23:07 +0100

    Creating data for each speech working, lines separate from stagedirs

--- search/samples/jsdom/index.js
+++ search/samples/jsdom/index.js
@@ -1,36 +1,51 @@
 const fs = require('fs');
-
 const {JSDOM} = require('jsdom');
+
+const abbreviations = require('./data/abbreviations.json');
 
 JSDOM.fromFile('../../originals/a_and_c.xml', {contentType: 'text/xml'})
 .then(dom => {
-  // const playTitle = $(dom, 'PLAY > TITLE').textContent;
-  // const playSubtitle = $('PLAYSUBT').textContent;
+  //  const playTitle = $(dom, 'PLAY > TITLE')[0].textContent;
+  const playSubtitle = $(dom, 'PLAY > PLAYSUBT')[0].textContent;
+  const playAbbreviation = abbreviations[playSubtitle];
   let docs = [];
   const acts = $(dom, 'ACT');
   let actNum = 0;
-  for (let act of acts) {
+  for (const act of acts) {
     actNum++;
     const scenes = $(act, 'SCENE');
-    // console.log(playTitle, actTitle, scenes.length);
+    // const actTitle = $(act, 'TITLE')[0].textContent;
     let sceneNum = 0;
-    for (let scene of scenes) {
+    for (const scene of scenes) {
+      // const sceneTitle = $(scene, 'TITLE'); // must be searchable
       sceneNum++;
+      const location = playAbbreviation + '.' + actNum + '.' + sceneNum;
       const stagedirs = $(scene, 'STAGEDIR');
       for (let stagedir of stagedirs) {
         docs.push({
-          l: actNum + '.' + sceneNum,
+          l: location,
           t: stagedir.textContent
         });
       }
-      console.log(docs);
+      const speeches = $(scene, 'SPEECH');
+      for (const speech of speeches) {
+        const speaker = $(speech, 'SPEAKER')[0];
+        const lines = [...$(speech, 'LINE')].map(line => line.textContent);
+        docs.push({
+          l: location,
+          s: speaker.textContent,
+          t: lines.join('\n')
+        });
+        // for (const line of lines) {
+        //   docs.push({
+        //     l: actNum + '.' + sceneNum,
+        //     s: speaker.textContent,
+        //     t: line.textContent
+        //   });
+        // }
+      }
     }
   }
-  // for each scene
-  // for each speech
-  // for each line or stagedir
-  // add id
-
 //  const output = dom.serialize();
   writeFile('output.json', JSON.stringify(docs));
 });
@@ -46,10 +61,10 @@ function writeFile(filepath, string) {
 
 // Utility functions
 
-function $(obj, selector) {
-  var results = obj.constructor.name === 'JSDOM' ?
-    obj.window.document.querySelectorAll(selector) :
-    obj.querySelectorAll(selector);
-  return results.length === 1 ? results[0] : results;
+// Return a nodeList of elements for ancestor element and selector
+function $(ancestor, selector) {
+  return ancestor.constructor.name === 'JSDOM' ?
+    ancestor.window.document.querySelectorAll(selector) :
+    ancestor.querySelectorAll(selector);
 }
 
