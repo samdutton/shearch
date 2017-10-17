8a3c4ef6951fd2c4b5c4384b5136729efe589e7f
Sam Dutton <samdutton@gmail.com>
2017-10-17 10:05:50 +0100

    Updates to tohtml

--- search/samples/tohtml/index.js
+++ search/samples/tohtml/index.js
@@ -2,13 +2,13 @@ const fs = require('fs');
 const mz = require('mz/fs');
 const {JSDOM} = require('jsdom');
 
-const abbreviations = require('./data/abbreviations.json');
+// const abbreviations = require('./data/abbreviations.json');
 const INPUT_DIR = '../../originals/';
-const OUTPUT_DIR = 'jsonout/';
+// const OUTPUT_DIR = 'htmlout/';
 
 mz.readdir(INPUT_DIR).then(filenames => {
   filenames = filenames.filter(filename => {
-    return filename.match(/.+xml/);
+    return filename.match(/^a_.+/); // .+xml
   });
   for (const filename of filenames) {
     processFile(filename);
@@ -19,69 +19,55 @@ mz.readdir(INPUT_DIR).then(filenames => {
 function processFile(filename) {
   JSDOM.fromFile(INPUT_DIR + filename, {contentType: 'text/xml'})
   .then(dom => {
-    //  const playTitle = $(dom, 'PLAY > TITLE')[0].textContent;
-    const playSubtitle = $(dom, 'PLAY > PLAYSUBT')[0].textContent;
-    const playAbbreviation = abbreviations[playSubtitle];
-    let docs = [];
-    const acts = $(dom, 'ACT');
-    let actNum = 0;
-    for (const act of acts) {
-//      console.log(act.innerHTML.match(/<stagedir>/g[0]));
-      actNum++;
-      const scenes = $(act, 'SCENE');
-      // const actTitle = $(act, 'TITLE')[0].textContent;
-      let sceneNum = 0;
-      for (const scene of scenes) {
-        // const sceneTitle = $(scene, 'TITLE'); // must be searchable
-        sceneNum++;
-        const location = playAbbreviation + '.' + actNum + '.' + sceneNum;
-        const stagedirs = $(scene, 'STAGEDIR');
-        for (let stagedir of stagedirs) {
-          docs.push({
-            l: location,
-            t: stagedir.textContent
-          });
-        }
-        const speeches = $(scene, 'SPEECH');
-        for (const speech of speeches) {
-          const speaker = $(speech, 'SPEAKER')[0];
-          const lines = [...$(speech, 'LINE')].map(line => line.textContent);
-          docs.push({
-            l: location,
-            s: speaker.textContent,
-            t: lines.join('\n')
-          });
-          // for (const line of lines) {
-          //   docs.push({
-          //     l: actNum + '.' + sceneNum,
-          //     s: speaker.textContent,
-          //     t: line.textContent
-          //   });
-          // }
-        }
+    // let htmldoc = '';
+    const play = dom.window.document.querySelector('PLAY');
+    const childNodes = play.childNodes;
+    for (const node of childNodes) {
+      console.log('childNode.nodeName', node.nodeName);
+      switch(node.nodeName) {
+      case 'TITLE':
+        console.log(node.textContent);
+        break;
+      case 'FM':
+        console.log(node.textContent);
+        break;
+      case 'PERSONAE':
+        console.log(node.childNodes.length);
+        break;
+      case 'SCNDESCR':
+        console.log(node.textContent);
+        break;
+      case 'PLAYSUBT':
+        console.log(node.textContent);
+        break;
+      case 'ACT':
+        console.log(node.childNodes.length);
+        break;
+      case '#text':
+        console.log(node.textContent);
+        break;
+      default:
+        console.error(`Node not found: ${node.nodeName}`);
       }
     }
-  //  const output = dom.serialize();
-    const outputPath = OUTPUT_DIR + filename.replace('xml', 'json');
-    console.log('Outputing file: ', outputPath);
-    writeFile(outputPath, JSON.stringify(docs));
   });
 }
 
-function writeFile(filepath, string) {
-  fs.writeFile(filepath, string, error => {
-    if(error) {
-      return console.error(error);
-    }
-    console.log(`The file ${filepath} was saved!`);
-  });
-}
+// function writeFile(filepath, string) {
+//   mz.writeFile(filepath, string).then(() => {
+//     console.log(`Success creating ${filepath}`);
+//   }).catch(error => console.error(`Error writing to ${filepath}:`, error));
+// }
 
 // Utility functions
 
 // Return a nodeList of elements for ancestor element and selector
-function $(ancestor, selector) {
-  return ancestor.constructor.name === 'JSDOM' ?
-    ancestor.window.document.querySelectorAll(selector) :
-    ancestor.querySelectorAll(selector);
-}
+// function $(ancestor, selector) {
+//   return ancestor.constructor.name === 'JSDOM' ?
+//     ancestor.window.document.querySelectorAll(selector) :
+//     ancestor.querySelectorAll(selector);
+// }
+
+// [...components].slice(0,50).forEach(function(component) {
+//   console.log(`${playTitle} component`, component.nodeName);
+// });
