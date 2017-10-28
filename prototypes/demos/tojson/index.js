fbdb2c38ac8bd9a6847c1ee21a2aba5b8f255320
Sam Dutton <samdutton@gmail.com>
2017-10-27 19:06:32 -0700

    Started work on JS to create index with Node

--- prototypes/demos/tojson/index.js
+++ prototypes/demos/tojson/index.js
@@ -1,55 +1,75 @@
-const fs = require('fs');
+const mz = require('mz/fs');
 const {JSDOM} = require('jsdom');
 
 const abbreviations = require('./data/abbreviations.json');
 
-JSDOM.fromFile('../../originals/a_and_c.xml', {contentType: 'text/xml'})
-.then(dom => {
-  //  const playTitle = $(dom, 'PLAY > TITLE')[0].textContent;
-  const playSubtitle = $(dom, 'PLAY > PLAYSUBT')[0].textContent;
-  const playAbbreviation = abbreviations[playSubtitle];
-  let docs = [];
-  const acts = $(dom, 'ACT');
-  let actNum = 0;
-  for (const act of acts) {
-    console.log(act.innerHTML.match(/<stagedir>/g[0]));
-    actNum++;
-    const scenes = $(act, 'SCENE');
-    // const actTitle = $(act, 'TITLE')[0].textContent;
-    let sceneNum = 0;
-    for (const scene of scenes) {
-      // const sceneTitle = $(scene, 'TITLE'); // must be searchable
-      sceneNum++;
-      const location = playAbbreviation + '.' + actNum + '.' + sceneNum;
-      const stagedirs = $(scene, 'STAGEDIR');
-      for (let stagedir of stagedirs) {
+const INPUT_DIR = '../../originals/';
+const OUTPUT_DIR = 'htmlout/';
+
+var docNum = 0;
+
+mz.readdir(INPUT_DIR).then(filenames => {
+  filenames = filenames.filter(filename => {
+    return filename.match(/.+xml/);
+  });
+  for (const filename of filenames) {
+    createDocs(filename);
+  }
+}).catch(error => console.error(`Error reading from ${INPUT_DIR}:`, error));
+
+
+function createDocs(filename) {
+  JSDOM.fromFile(filename, {contentType: 'text/xml'})
+  .then(dom => {
+    const play = dom.window.document.querySelectorAll('PLAY');
+    const playAbbreviation = abbreviations[filename];
+    let docs = [];
+    const acts = play.querySelectorAll('ACT');
+    for (let actNum = 1; actNum <= acts.length; ++actNum) {
+      const act = acts[actNum -1];
+      const scenes = act.querySelectorAll('SCENE');
+      for (let sceneNum = 1; sceneNum <= scenes.length; ++sceneNum) {
+        const scene = scenes[sceneNum - 1];
+        const location = playAbbreviation + '.' + actNum + '.' + sceneNum;
+        const sceneTitle = scene.querySelector('TITLE').textContent;
         docs.push({
+          n: docNum++,
           l: location,
-          t: stagedir.textContent
+          t: sceneTitle
         });
-      }
-      const speeches = $(scene, 'SPEECH');
-      for (const speech of speeches) {
-        const speaker = $(speech, 'SPEAKER')[0];
-        const lines = [...$(speech, 'LINE')].map(line => line.textContent);
-        docs.push({
-          l: location,
-          s: speaker.textContent,
-          t: lines.join('\n')
-        });
-        // for (const line of lines) {
-        //   docs.push({
-        //     l: actNum + '.' + sceneNum,
-        //     s: speaker.textContent,
-        //     t: line.textContent
-        //   });
-        // }
+        const stagedirs = scene.querySelectorAll('div.stage-direction');
+        for (const stagedir of stagedirs) {
+          docs.push({
+            n: docNum++,
+            l: location,
+            t: stagedir.textContent
+          });
+        }
+        const speeches = scene.querySelectorAll('ol.speech');
+        for (const speech of speeches) {
+          const speaker = speech.querySelector('li.speaker').textContent;
+          const lines = speech.querySelectorAll('li:not(.speaker)');
+          for (const line of lines) {
+            docs.push({
+              n: docNum++,
+              l: location,
+              s: speaker,
+              t: line.textContent
+            });
+          }
+        }
       }
     }
-  }
-//  const output = dom.serialize();
-  writeFile('output.json', JSON.stringify(docs));
-});
+  });
+}
+
+
+  // indexDocs(playAbbreviation, docs);
+
+  // writeFile('output.json', JSON.stringify(docs));
+
+// writeFile('output.json', JSON.stringify(docs));
+
 
 function writeFile(filepath, string) {
   fs.writeFile(filepath, string, error => {
@@ -58,14 +78,5 @@ function writeFile(filepath, string) {
     }
     console.log(`The file ${filepath} was saved!`);
   });
-}
-
-// Utility functions
-
-// Return a nodeList of elements for ancestor element and selector
-function $(ancestor, selector) {
-  return ancestor.constructor.name === 'JSDOM' ?
-    ancestor.window.document.querySelectorAll(selector) :
-    ancestor.querySelectorAll(selector);
 }
 
