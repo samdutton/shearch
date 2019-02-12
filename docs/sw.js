b3ca37737639a842484760b50642adab14ab30be
Sam Dutton <samdutton@gmail.com>
2019-02-12 12:33:50 +0000

    Added Workbox!

--- docs/sw.js
+++ docs/sw.js
@@ -18,11 +18,11 @@ limitations under the License.
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
 
-if (workbox) {
-  console.log(`Yay! Workbox is loaded 🎉`);
-} else {
-  console.log(`Boo! Workbox didn't load 😬`);
-}
+// if (workbox) {
+//   console.log(`Yay! Workbox is loaded 🎉`);
+// } else {
+//   console.log(`Boo! Workbox didn't load 😬`);
+// }
 
 workbox.routing.registerRoute(
   /.*\.(?:css|html|json|js)/,
